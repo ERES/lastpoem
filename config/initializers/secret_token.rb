dfac4ac8c3b1cab792386afd44ad0e5204525522
Ernesto Espinoza <espinoza.ern@gmail.com>
2013-12-20 14:52:00 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lastpoem::Application.config.secret_token = '6d08162ca73723b0b783f730b51f8413ebc3fb281cc9241600a05713b8b7d5270dcea139803cda3463a8808f1ff5a7b618cee6f9e5555027b5afbbf34d9186f7'
